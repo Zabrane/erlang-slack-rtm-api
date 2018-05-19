159fd91dccfa0265e10213933decb3696570b3fd
Ross Schlaikjer <ross@schlaikjer.net>
2018-05-19 22:47:18 +0000

    New connect method

--- src/slack_rtm.erl
+++ src/slack_rtm.erl
@@ -1,13 +1,15 @@
 -module(slack_rtm).
 -compile([{parse_transform, lager_transform}]).
--export([connect/1]).
+-export([connect/1, connect/2]).
 -export([printer/1, printer_loop/1, printer_loop/0]).
 
 -spec connect(binary() | list()) -> {ok, pid()} | {error, any()}.
-connect(Token) when is_list(Token) ->
+connect(Token) ->
+    connect(Token, record).
+connect(Token, Mode) when is_list(Token) ->
     connect(list_to_binary(Token));
-connect(Token) when is_binary(Token) ->
-    supervisor:start_child(slack_rtm_sup, [self(), Token]).
+connect(Token, Mode) when is_binary(Token) ->
+    supervisor:start_child(slack_rtm_sup, [self(), Token, Mode]).
 
 printer_loop(Token) ->
     {ok, _} = connect(Token),
